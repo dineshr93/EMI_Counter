2f6362fa286d57a5d443b203123716b905660451
Ravi Dinesh <Dinesh.Ravi@elektrobit.com>
2019-10-02 21:26:59 +0530

    initial stable commit

---
+++ src/com/din/sw/EMIComputer.java
@@ -0,0 +1,227 @@
+package com.din.sw;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.text.ParseException;
+import java.time.YearMonth;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+
+
+
+//https://www.mkyong.com/java/how-to-convert-java-object-to-from-json-jackson/
+// 2015-12 to 2029-12
+//169 Total
+//47 completed
+//122 Pending
+
+//09-19 to 09-22
+
+public class EMIComputer {
+
+	public static void main(String[] args) throws ParseException, IOException { 
+
+		String name = "Dinesh";
+		Purpose purpose = Purpose.Car;
+		YearMonth current = YearMonth.now(ZoneId.of(ZoneOffset.systemDefault().toString()));
+		YearMonth start = YearMonth.parse("2019-09");
+		YearMonth end = YearMonth.parse("2022-09");
+
+
+		LinkedHashMap<YearMonth,Integer> map = new LinkedHashMap<>();
+
+
+		ArrayList<YearMonth> completedList = new ArrayList<YearMonth>();
+		ArrayList<YearMonth> pendingList = new ArrayList<YearMonth>();
+
+		YearMonth ym = start ;
+		int count = 0;
+		while(!ym.isAfter(end)) {
+			map.put( ym, ++count );
+			ym = ym.plusMonths( 1 ); 
+		}
+
+		int totalEMIs = map.size();
+		int completed = map.get(current); 
+		int pending = totalEMIs - completed;
+
+
+
+
+		//System.out.println("Completed Months");
+		ym = start;
+		count = 0;
+		while(!ym.isAfter(current)) {
+			completedList.add(ym);
+			ym = ym.plusMonths( 1 ); 
+		}
+
+		//System.out.println("Pending Months");
+		ym = current.plusMonths(1);
+		count = 0;
+		while(!ym.isAfter(end)) {
+			pendingList.add(ym);
+			ym = ym.plusMonths( 1 ); 
+		}
+		
+		
+		PrintStream fileOut = new PrintStream(name+"_"+purpose+"_EMI"+".txt");
+		System.setOut(fileOut);
+		File file = new File(name+"_"+purpose+"_EMI"+".txt");
+
+
+		System.out.println(name+"_"+purpose+"_"+"EMI_Report");
+		System.out.println("======================");
+		System.out.println("Starts     "+ start.getYear()+"-"+start.getMonth());
+		System.out.println("Until      "+ end.getYear()+"-"+end.getMonth());
+		System.out.println("Total EMIs "+totalEMIs);
+		System.out.println("Completed  "+ completed );
+		System.out.println("Pending    "+pending);
+		System.out.println();
+		System.out.println("======================");
+		System.out.println("Completed Months");
+		System.out.println("======================");
+		printList(completedList);
+		System.out.println();
+		System.out.println("======================");
+		System.out.println("Pending Months");
+		System.out.println("======================");
+		printList(pendingList);
+
+		EMIData data = new EMIData();
+		data.setName(name);
+		data.setPurpose(purpose);
+		data.setStart(start);
+		data.setEnd(end);
+		data.setTotalEMIs(totalEMIs);
+		data.setCompleted(completed);
+		data.setPending(pending);
+		data.setMap(map);
+		data.setCompletedList(completedList);
+		data.setPendingList(pendingList);
+		File jsonFile = new File(name+"_"+purpose+"_EMI"+".json");
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.findAndRegisterModules();
+		try {
+			String json = mapper.writeValueAsString(data);
+			//System.out.println("ResultingJSONstring = " + json);
+			//JSON from file to Object
+			//EMIData eMIData = mapper.readValue(new File("c:\\user.json"), EMIData.class);
+
+			//JSON from String to Object
+			EMIData eMIData = mapper.readValue(json, EMIData.class);
+			
+			FileUtils.writeStringToFile(jsonFile, json, "ISO-8859-1");
+			//System.out.println(eMIData.getStart());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("error");
+		}
+		Desktop desktop = Desktop.getDesktop();
+        if(file.exists()) desktop.open(file);
+        if(jsonFile.exists()) desktop.open(jsonFile);
+        
+	}
+	private static void printList(List<YearMonth> myList) {
+		int count = 0;
+		for (YearMonth string : myList) {
+			System.out.println(++count + " "+ string.getYear()+"-"+string.getMonth());
+		}
+	}
+}
+enum Purpose{
+	Car,
+	Home,
+	Mobile,
+	Personal,
+	Insurance,
+	Pension
+}
+class EMIData {
+	String name;
+	Purpose purpose;
+	int totalEMIs;
+	int completed; 
+	int pending;
+	YearMonth start;
+	YearMonth end;
+	LinkedHashMap<YearMonth,Integer> map;
+	ArrayList<YearMonth> completedList;
+	ArrayList<YearMonth> pendingList;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Purpose getPurpose() {
+		return purpose;
+	}
+	public void setPurpose(Purpose purpose) {
+		this.purpose = purpose;
+	}
+	public int getTotalEMIs() {
+		return totalEMIs;
+	}
+	public void setTotalEMIs(int totalEMIs) {
+		this.totalEMIs = totalEMIs;
+	}
+	public int getCompleted() {
+		return completed;
+	}
+	public void setCompleted(int completed) {
+		this.completed = completed;
+	}
+	public int getPending() {
+		return pending;
+	}
+	public void setPending(int pending) {
+		this.pending = pending;
+	}
+	public YearMonth getStart() {
+		return start;
+	}
+	public void setStart(YearMonth start) {
+		this.start = start;
+	}
+	public YearMonth getEnd() {
+		return end;
+	}
+	public void setEnd(YearMonth end) {
+		this.end = end;
+	}
+	public LinkedHashMap<YearMonth, Integer> getMap() {
+		return map;
+	}
+	public void setMap(LinkedHashMap<YearMonth, Integer> map) {
+		this.map = map;
+	}
+	public ArrayList<YearMonth> getCompletedList() {
+		return completedList;
+	}
+	public void setCompletedList(ArrayList<YearMonth> completedList) {
+		this.completedList = completedList;
+	}
+	public ArrayList<YearMonth> getPendingList() {
+		return pendingList;
+	}
+	public void setPendingList(ArrayList<YearMonth> pendingList) {
+		this.pendingList = pendingList;
+	}
+
+}
